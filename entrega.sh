2cb35229ece906fcc938a6ada82d524cb5f5f0a9
icalamesa <icalamesa@gmail.com>
2021-04-27 22:57:47 +0200

    script chachi

--- entrega.sh
+++ entrega.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-
 extensions=".cc .hh"
 paths=""
+rm -fr $1
+mkdir $1
 for i in ${extensions}
 do
     paths="${paths} `find -name *${i}`"
 done
-echo "ivan ${paths}"
+for i in ${paths}
+do
+    cp ${i} ${1}/
+done
+tar -cvf entrega.tar $1
